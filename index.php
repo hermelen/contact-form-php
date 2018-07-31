dd24ecf8ee6459951736cd09b1f371baf4737608
hermelen <hermelen.peris@gmail.com>
2018-07-31 12:26:43 +0200

    finish

--- index.php
+++ index.php
@@ -1,10 +1,58 @@
 <?php require "header.php"; ?>
 <?php require "setting.php"; ?>
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 
 
+require 'vendor/autoload.php';
 
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = $userName;                          // SMTP username
+    $mail->Password = $pass;                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
 
+    //Recipients
+    $mail->setFrom($userName, 'contact-form-php');
+    $mail->addAddress($userName, 'Moi');     // Add a recipient
+    // $mail->addAddress('ellen@example.com');               // Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('media/codi_rabbit.jpg');         // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Message from contact-form-php';
+    $mail->Body    = 'Body';
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
 
 ?>
+<form class="" action="index.php" method="post" enctype="multipart/form-data">
+  <label for="email">Votre email</label>
+  <input type="text" name="email" value="">
+  <label for="message">Votre message</label>
+  <textarea name="message" rows="8" cols="80"></textarea>
+  <input type="submit" name="" value="">
+</form>
  <?php require "footer.php"; ?>
